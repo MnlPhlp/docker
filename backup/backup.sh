5f28ab3fab15ac5229f605e77ec5b3a9a6616a33
MnlPhlp <online@manuel.sh>
2020-10-25 14:43:39 +0100

    .

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,5 +2,5 @@
 # first parameter needs to be the path of the folders to backup
 cd $2
 for filename in *; do \
-    echo tar -czf "$1/`basename $filename`.tar.gz" "$filename"
+    tar -czf "$1/`basename $filename`.tar.gz" "$filename"
 done
