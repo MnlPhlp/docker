dd159727dda02099b03e86f9ac4c0af8afa04761
MnlPhlp <online@manuel.sh>
2020-10-25 14:39:52 +0100

    small fixes

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,5 +1,6 @@
 # first parameter needs to be the path of the created tar files
 # first parameter needs to be the path of the folders to backup
-for filename in $2/*; do \
+cd $2
+for filename in *; do \
     echo tar -czf "$1/`basename $filename`.tar.gz" "$filename"
 done
