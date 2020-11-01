706d96120efb27e73990be5402e3ac8e96d2ab76
MnlPhlp <online@manuel.sh>
2020-11-01 22:31:03 +0100

    .

--- nextcloud/setup.sh
+++ nextcloud/setup.sh
@@ -1,2 +1,3 @@
 docker-compose exec --user www-data app php occ maintenance:install --database "pgsql" --database-host "db" --database-name "nextcloud"  --database-user "nextcloud" --database-pass "nextcloud" --admin-user "<user>" --admin-pass "<passwd>"
 docker-compose exec --user www-data app php occ config:system:set overwriteprotocol --value="https"
+docker-compose exec --user www-data app php occ config:system:set trusted_domains 2 --value=data.manuel.sh
